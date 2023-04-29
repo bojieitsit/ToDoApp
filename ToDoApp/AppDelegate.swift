9e69902785e49b1c0a22250c81bbb4460210f1a0
Andrey Bogoslovskiy <88778576+bojieitsit@users.noreply.github.com>
2023-04-29 17:12:08 +0400

    Crashlytics added

--- ToDoApp/AppDelegate.swift
+++ ToDoApp/AppDelegate.swift
@@ -6,8 +6,8 @@
 //
 
 import UIKit
-import CoreData
 import RealmSwift
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,15 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        //print(Realm.Configuration.defaultConfiguration.fileURL)
-        
+        FirebaseApp.configure()
         do {
             _ = try Realm()
         } catch {
             print("Error initialising new realm, \(error)")
         }
-        
         
         return true
     }
